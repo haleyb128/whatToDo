2548e5317398943fa104aba562a460b0a24df619
HaleyB <haleyb128@gmail.com>
2019-04-22 13:13:58 -0500

    getting started with pseudo code

---
+++ app.js
@@ -0,0 +1,20 @@
+//Declare variables
+
+//Form submit functionality on "submit"
+    //form validation
+        //verify task name
+        //verify date; after today
+    //calculate "daysToGo"
+        //date input - current date = daysLeft
+        
+    //bring in form variables
+    //append to "toast"
+        //color based on daysLeft
+            //default grey
+            //if less than 7 days: yellow
+            //if less than 3 days: red
+    //append toast to "to-do" section
+
+
+//Interaction with toasts
+    //delete when "x" is clicked
